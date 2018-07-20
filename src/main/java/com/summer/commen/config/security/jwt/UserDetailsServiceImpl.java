c92aeca6a739499ecd6e1a0a1081d3cf01fc25d8
Chenxicheng <850068241@qq.com>
2018-07-20 17:59:27 +0800

    spring security jwt 添加

---
+++ src/main/java/com/summer/commen/config/security/jwt/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.summer.commen.config.security.jwt;
+
+import com.summer.modules.sys.entity.User;
+import com.summer.modules.sys.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * 从用户名可以查到用户并返回的方法
+ */
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.getByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(String.format("No user found with username '%s'.", username));
+        } else {
+            return SecurityUserDetailFactory.create(user);
+        }
+    }
+}
