ec045b6e0298c8d09cda033582007abcd7ac2b5e
Dashwood <cxc_dashwood@163.com>
2018-07-26 22:59:41 +0800

    添加方法

--- src/main/java/com/summer/modules/sys/service/PermissionService.java
+++ src/main/java/com/summer/modules/sys/service/PermissionService.java
@@ -3,10 +3,20 @@ package com.summer.modules.sys.service;
 import com.summer.commen.base.BaseService;
 import com.summer.modules.sys.entity.Permission;
 
+import java.util.List;
+
 /**
  * @Author: Dashwood
  * @Date: 2018/7/21 15:01
  * @Version: 1.0
  */
 public interface PermissionService extends BaseService<Permission>{
+
+    /**
+     * 根据用户id获取该用户所拥有权限
+     * @param userId
+     * @return
+     */
+    List<Permission> findMenuByUserId (String userId);
+
 }
--- src/main/java/com/summer/modules/sys/service/impl/PermissionServiceImpl.java
+++ src/main/java/com/summer/modules/sys/service/impl/PermissionServiceImpl.java
@@ -4,8 +4,12 @@ import com.summer.commen.base.AbstractBaseService;
 import com.summer.modules.sys.dao.PermissionDao;
 import com.summer.modules.sys.entity.Permission;
 import com.summer.modules.sys.service.PermissionService;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * @Author: Dashwood
@@ -13,6 +17,15 @@ import org.springframework.transaction.annotation.Transactional;
  * @Version: 1.0
  */
 @Service
+@CacheConfig(cacheNames = "permission")
 @Transactional(readOnly = true)
 public class PermissionServiceImpl extends AbstractBaseService<PermissionDao, Permission> implements PermissionService {
+
+    @Override
+    @Cacheable(key = "'menuList:'+#userId")
+    public List<Permission> findMenuByUserId(String userId) {
+        List<Permission> permissionList = dao.findListByUserId(userId);
+
+        return null;
+    }
 }
